ef07daceb9b45fd920277d11c61989a3c2c422e7
Leandro Blandi <leandronahuelblandiomg@gmail.com>
2022-10-22 22:55:23 -0300

    Testing TDA functions

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "producto.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    Producto producto = crearProducto("Coca Cola", 1500, 6);
+    mostrarProducto(producto);
+
     return 0;
 }
